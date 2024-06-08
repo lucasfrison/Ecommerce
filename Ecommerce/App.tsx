fab0907c222718637d60511957fa3e01c423cd06
gafonso <91420802+SelectGabriel@users.noreply.github.com>
2024-06-08 18:29:30 -0300

    Update App.tsx

--- Ecommerce/App.tsx
+++ Ecommerce/App.tsx
@@ -1,45 +1,71 @@
-import { Button } from '@react-native-material/core';
+import {Avatar, Button} from '@react-native-material/core';
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from "react";
-import {StyleSheet, View } from 'react-native';
+import {StyleSheet, Text, TextComponent, TextInput, View} from 'react-native';
 import ProductListScreen from "./src/components/ItemsList";
 import {
-  Backdrop,
-  BackdropSubheader,
-  AppBar,
-  IconButton,
+    Backdrop,
+    BackdropSubheader,
+    AppBar,
+    IconButton,
 } from "@react-native-material/core";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 
 
 export default function App() {
-  const [revealed, setRevealed] = useState(false);
-  return (
-      <View style={styles.container}>
-          <Backdrop
-              revealed={revealed}
-              header={
-                  <AppBar
-                      title="Screen title"
-                      transparent
-                      leading={props => (
-                          <IconButton
-                              icon={props => (
-                                  <Icon name={revealed ? "close" : "menu"} {...props} />
-                              )}
-                              onPress={() => setRevealed(prevState => !prevState)}
-                              {...props}
-                          />
-                      )}
-                  />
-              }
-              backLayer={<View style={{ height: 120 }} />}
-          >
-              <BackdropSubheader title="Produtos" />
-              <ProductListScreen></ProductListScreen>
-          </Backdrop>
-      </View>
-  );
+    const [revealed, setRevealed] = useState(false);
+    const [loggedIn, setLoggedIn] = useState(false);
+    return (
+        <View style={styles.container}>
+            <Backdrop
+                revealed={revealed}
+                header={
+                    <AppBar
+                        title="Screen title"
+                        transparent
+                        leading={props => (
+                            <IconButton
+                                icon={props => (
+                                    <Icon name={revealed ? "close" : "menu"} {...props} />
+                                )}
+                                onPress={() => setRevealed(prevState => !prevState)}
+                                {...props}
+
+                            />
+                        )}
+                        trailing={props =>
+                            loggedIn ? (
+                                <IconButton
+                                    icon={<Avatar label="Yaman KATBY" size={28} />}
+                                    onPress={() => setLoggedIn(!loggedIn)}
+                                    {...props}
+                                />
+                            ) : (
+                                <Button
+                                    variant="text"
+                                    title="Login"
+                                    compact
+                                    style={{ marginEnd: 4 }}
+                                    onPress={() => setLoggedIn(!loggedIn)}
+                                    {...props}
+                                />
+                            )
+                        }
+                    />
+                }
+                backLayer={<View style={{ height: 120 }}>
+                    <View>
+                        <IconButton
+                            icon={<Avatar label="Yaman KATBY" size={118} />}
+                        />
+                    </View>
+                </View>}
+            >
+                <BackdropSubheader title="Produtos" />
+                <ProductListScreen></ProductListScreen>
+            </Backdrop>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
