a876be5d3bc7881665927ac5c597f89769f49a19
niscola29 <nicolasportela48@gmail.com>
2024-06-16 20:36:43 -0300

    [ADD] Listagem de Produtos e Detalhes integrados com o back

--- Ecommerce/src/screens/ProductDetailScreen.tsx
+++ Ecommerce/src/screens/ProductDetailScreen.tsx
@@ -1,35 +1,37 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
 import { RouteProp, useRoute } from '@react-navigation/native';
 import { Button } from "@react-native-material/core";
 
 type RootStackParamList = {
-  ProductDetail: { productId: number, name: string, price: string, image: string };
+  ProductDetail: { productId: string, name: string, price: string, description: string, images: string[] | null };
 };
 
 type ProductDetailScreenRouteProp = RouteProp<RootStackParamList, 'ProductDetail'>;
 
 const ProductDetailScreen: React.FC = () => {
   const route = useRoute<ProductDetailScreenRouteProp>();
-  const { productId, name, price, image } = route.params;
+  const { productId, name, price, description, images } = route.params;
 
   const product = {
     id: productId,
     name: name,
-    description: `O Playstation 3 foi o principal console da Sony entre os anos de 2006 e 2013, quando surgiu o Playstation 4. Ele foi um grande marco na indústria dos jogos, tanto pela durabilidade, mas também pela oferta de títulos exclusivos. Ele teve 3 modelos ao todo, indo do original (fat), ao slim e até ao super slim, no final de sua vida útil. Entre os principais games do Playstation 3, podemos citar The Last of Us, God of War 3, Uncharted 2 e GTA V. Seu controle, Dualshock 3, possuía um sensor de movimento que detectava alguns gestos nos controles, mas nunca foi de fato bem empregado nos jogos, sendo apenas um diferencial. O Playstation 3 foi o primeiro console da gigante japonesa a adotar Blu-ray como mídia padrão, o que elevou a qualidade dos jogos, tanto em gráfico, mas também em complexidade e recursos. Este console ainda servia como central de mídias, permitindo assistir à Blu-Ray e acessar internet, via cabo ou WiFi.`,
-    image: image,
+    description: description,
+    images: images,
     price: price,
-  };
-
-  const handleAddToCart = () => {
-
   };
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.name}>{product.name}</Text>
-      <Image source={{ uri: product.image, headers: { 'Accept': 'image/*' } }} style={styles.image} />
-      <Text style={styles.price}>{product.price}</Text>
+      {product.images && product.images.length > 0 ? (
+        <Image source={{ uri: product.images[0] }} style={styles.image} />
+      ) : (
+        <View style={[styles.image, styles.noImage]}>
+          <Text>No Image Available</Text>
+        </View>
+      )}
+      <Text style={styles.price}>R$ {product.price}</Text>
       <Text style={styles.bold}>Em até 10x sem juros</Text>
       <Text>Entrega Grátis em: <Text style={styles.bold}>7 dias úteis</Text></Text>
       <Text style={styles.description}>{product.description}</Text>
@@ -71,14 +73,11 @@ const styles = StyleSheet.create({
   bold: {
     fontWeight: 'bold',
   },
-  addToCartButton: {
-    borderRadius: 10,
-    paddingVertical: 15,
+  noImage: {
+    backgroundColor: '#eee',
+    justifyContent: 'center',
     alignItems: 'center',
-  },
-  addToCartButtonText: {
-    fontSize: 18,
-    fontWeight: 'bold',
+    height: 300, // Ajuste conforme necessário
   },
 });
 
